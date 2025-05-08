da261a629197579cd19f5e7c6565d8af40508409
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-05-08 16:50:20 -0300

    feat: add exception handler for object not found

--- Help Desk 2.0/user-service-api/src/main/java/br/com/pauloultra/userserviceapi/service/UserService.java
+++ Help Desk 2.0/user-service-api/src/main/java/br/com/pauloultra/userserviceapi/service/UserService.java
@@ -3,6 +3,7 @@ package br.com.pauloultra.userserviceapi.service;
 import br.com.pauloultra.userserviceapi.mapper.UserMapper;
 import br.com.pauloultra.userserviceapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import models.exceptions.ResourceNotFoundException;
 import models.reponses.UserResponse;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class UserService {
     private final UserMapper userMapper;
 
     public UserResponse findById(final String id) {
-        return userMapper.fromEntity(userRepository.findById(id).orElse(null));
+        return userMapper.fromEntity(userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
+                "Object not found. Id: " + id + ", Type: " + UserResponse.class.getSimpleName())));
     }
 }
