360528e384b53e6dde27ce7c10b7e41fbbac825b
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-05-16 16:43:38 -0300

    feature: add jakarta bean validation

--- Help Desk 2.0/user-service-api/src/main/java/br/com/pauloultra/userserviceapi/controller/UserController.java
+++ Help Desk 2.0/user-service-api/src/main/java/br/com/pauloultra/userserviceapi/controller/UserController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import models.exceptions.StandardError;
 import models.reponses.UserResponse;
 import models.requests.CreateUserRequest;
@@ -54,6 +55,6 @@ public interface UserController {
     })
     @PostMapping
     ResponseEntity<Void> save(
-            @RequestBody final CreateUserRequest createUserRequest
+            @Valid @RequestBody final CreateUserRequest createUserRequest
             );
 }
