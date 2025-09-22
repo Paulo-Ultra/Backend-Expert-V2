4a0928d9eab52a601ceaffd3e86c472771c4648e
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2025-09-22 15:14:59 -0300

    feat: add authentication impl

---
+++ Help Desk 2.0/auth-service-api/src/main/java/br/com/pauloultra/authserviceapi/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,56 @@
+package br.com.pauloultra.authserviceapi.controller.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import models.exceptions.StandardError;
+import models.exceptions.ValidationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.ArrayList;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    ResponseEntity<StandardError> handleNotFoundException(final UsernameNotFoundException ex,
+                                              final HttpServletRequest request) {
+
+        return ResponseEntity.status(NOT_FOUND).body(
+                StandardError.builder()
+                        .timestamp(now())
+                        .status(NOT_FOUND.value())
+                        .error(NOT_FOUND.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .path(request.getRequestURI())
+                    .build()
+        );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ValidationException> handleMethodArgumentNotValidException(final MethodArgumentNotValidException ex,
+                                                          final HttpServletRequest request) {
+
+        var errors = ValidationException.builder()
+                .timestamp(now())
+                .status(BAD_REQUEST.value())
+                .error("Validation Exception")
+                .message("Exception in validation attributes")
+                .path(request.getRequestURI())
+                .fieldErrors(new ArrayList<>())
+                .build();
+
+        for(FieldError error : ex.getBindingResult().getFieldErrors()) {
+            errors.addError(error.getField(), error.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
